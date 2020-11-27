b965f6c740d296993db97240c5ecc3e30c3a019a
vignesh sweekaran <vigneshsweekaran@gmail.com>
2020-11-27 21:42:24 +0530

    updated

---
+++ jenkins/setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+NAMESPACE="jenkins"
+RELEASE_NAME="jenkins"
+
+# Adding jenkins helm repo and updating the repo
+if [[ ! `helm repo list | awk '{if($1 == "jenkins") print "found";}'` == "found" ]]; then
+    helm repo add jenkins https://charts.jenkins.io
+    helm repo update
+fi
+
+# Creating namespace for jenkins
+if [[ ! `kubectl get ns | awk '{if($1 == $NAMESPACE) print "found";}'` == "found" ]]; then
+    kubectl create ns $NAMESPACE
+fi
+
+# Install helm chart
+if [[ ! `helm list -n "$namespace" | awk '{if($1 == "jenkins") print "found";}'` == "found" ]]; then
+    helm install $RELEASE_NAME -f values.yaml jenkins/jenkins
+fi
