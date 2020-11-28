1883513ac244cbf64ebfa027fd876c6e7218880b
vagrant <vagrant@kmaster.example.com>
2020-11-28 18:10:10 +0000

    updated setup.sh

--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -3,6 +3,12 @@
 NAMESPACE="jenkins"
 RELEASE_NAME="jenkins"
 
+# Install helm version 3 if not installed
+helm version
+if [[ $? != 0 ]]; then
+    curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+fi 
+
 # Adding jenkins helm repo and updating the repo
 if [[ ! `helm repo list | awk '{if($1 == "jenkins") print "found";}'` == "found" ]]; then
     helm repo add jenkins https://charts.jenkins.io
@@ -17,4 +23,9 @@ fi
 # Install helm chart
 if [[ ! `helm list -n $NAMESPACE | awk -v releaseName=$RELEASE_NAME '{if($1 == releaseName) print "found";}'` == "found" ]]; then
     helm install $RELEASE_NAME -f values.yaml jenkins/jenkins -n $NAMESPACE
+fi
+
+# Uninstalling jenkins helm release Release
+if [[ $* == *uninstall* ]]; then
+    helm uninstall $RELEASE_NAME -n $NAMESPACE
 fi
